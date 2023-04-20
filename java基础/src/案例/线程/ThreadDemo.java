69db0dcd419ad6ccde8d7cd3fbd71caf19e34e10
bin_laptop <230453176@qq.com>
2023-04-20 10:26:31 +0800

    多线程

---
+++ java基础/src/案例/线程/ThreadDemo.java
@@ -0,0 +1,58 @@
+package java基础.src.案例.线程;
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(this.getName() + ":" + i);
+        }
+        System.out.println(this.getName() + "执行结束-------------------");
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("线程2：" + i);
+        }
+        System.out.println("线程执行结束------------------");
+    }
+}
+
+/**
+ * 当前的主线程就是main函数
+ * main函数和两个Thread并发执行
+ * 主线程执行完后，守护线程也会很快结束（不是立刻结束）
+ */
+
+
+public class ThreadDemo {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread t1 = new MyThread();
+        t1.setName("线程1");
+        Thread t2 = new Thread(new MyRunnable());
+        t2.setName("线程2");
+
+        //将t1,t2设置为守护线程
+        t1.setDaemon(true);
+        t2.setDaemon(true);
+
+        //开始执行
+        t1.start();
+        t2.start();
+        t1.join();//，此处程序阻塞，知道线程t1执行完成，程序才继续往下执行
+
+        //设置当前主线程name
+        Thread.currentThread().setName("主线程main");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ":" + i);
+        }
+        System.out.println("主线程执行结束-------------------");
+        //运行到这里代表主程序已经结束，但是其他线程还在运行，如果全部都是守护线程，那么程序很快就会退出
+
+    }
+}
--- java基础/src/线程Thread/线程控制/ThreadDaemonDemo.java
+++ java基础/src/线程Thread/线程控制/ThreadDaemonDemo.java
@@ -1,7 +1,7 @@
 package java基础.src.线程Thread.线程控制;
 
 /*
-    void setDaemon(boolean on):将此线程标记为守护线程，当运行的线程都是守护线程时，Java虚拟机将退出
+    void setDaemon(boolean true):将此线程标记为守护线程，当运行的线程都是守护线程时，Java虚拟机将退出
     主线程执行完后，守护线程很快也会停止执行（不是立刻停止，刘备死后，关羽和张飞也很快死了）
  */
 
@@ -25,6 +25,7 @@ public class ThreadDaemonDemo {
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName() + ":" + i);
         }
+        System.out.println("主线程执行完成");
 
     }
 }
