e9f073d6db79605cd6bf5ed3e304de85bbbe54a1
曹利彬 <1265501579@qq.com>
2023-04-22 13:21:14 +0800

    实验报告

--- Algorithm/src/LeetCode/Duplicate.java
+++ Algorithm/src/LeetCode/Duplicate.java
@@ -1,32 +1,38 @@
 package Algorithm.src.LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Duplicate {
     /*给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false*/
     public static void main(String[] args) {
-
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(containsDuplicate(new int[]{1, 2, 2, 3, 4, 5}));
     }
 
-    public boolean containsDuplicate(int[] nums) {
-        if (nums.length == 1) {
+    public static boolean containsDuplicate(int[] nums) {
+        if (nums.length <= 1) {
             return false;
         }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                map.compute(num, (k, v) -> v + 1);
-            } else {
-                map.put(num, 1);
-            }
+        Set<Integer> s = new HashSet<>();
+        for (int i : nums) {
+            s.add(i);
         }
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            if (e.getValue() > 1) {
+        return nums.length != s.size();
+    }
+
+    //对数组排序后判断相邻数据是否相同
+    public static boolean containsDuplicateII(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
-
         return false;
     }
 }
--- java基础/src/案例/实验报告/实验五/Ex2.java
+++ java基础/src/案例/实验报告/实验五/Ex2.java
@@ -1,16 +1,17 @@
 package java基础.src.案例.实验报告.实验五;
 
 /**
- 2. 编写名为IParent的接口，其中声明add(String s1,String s2)和minus(String s1,String s2)方法，返回值均为int型；编写Child类继承IParent接口，并实现add和minus方法，要求当主方法中执行如下代码时：
- IParent c=new Child();
- System.out.println(c.add("12","34"));
- System.out.println(c.minus("123","12"));		结果为：
- 46
- 111
+ * 2. 编写名为IParent的接口，其中声明add(String s1,String s2)和minus(String s1,String s2)方法，返回值均为int型；编写Child类继承IParent接口，并实现add和minus方法，要求当主方法中执行如下代码时：
+ * IParent c=new Child();
+ * System.out.println(c.add("12","34"));
+ * System.out.println(c.minus("123","12"));		结果为：
+ * 46
+ * 111
  */
 
 interface IParent {
     int add(String s1, String s2);
+
     int minus(String s1, String s2);
 }
 
@@ -29,6 +30,7 @@ class Child implements IParent {
 public class Ex2 {
     public static void main(String[] args) {
         IParent c = new Child();
+        System.out.println("结果为：");
         System.out.println(c.add("12", "34"));
         System.out.println(c.minus("123", "12"));
     }
